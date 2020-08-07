070aa48f9cf1f684803f3d4637d4ea7143dacec3
ibnukhawa <firstibnu@gmail.com>
2020-08-07 11:16:16 +0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+  'name': 'Innograph Warranty Serial Number',
+  'author': 'By Innograph Odoo Developers',
+  'version': '0.1',
+  'depends': [
+    # 'nama_modul',
+  ],
+  'data': [
+    # 'views/nama_file.xml',
+  ],
+  'qweb': [
+    # 'static/src/xml/nama_widget.xml',
+  ],
+  'sequence': 1,
+  'auto_install': False,
+  'installable': True,
+  'application': True,
+  'category': 'Sales Warranty',
+  'summary': 'by Ibnu Nur Khawarizmi & Bayu Dwi Saputra',
+  'license': 'OPL-1',
+  'website': 'https://www.innograph.com/',
+  'description': '-'
+}
